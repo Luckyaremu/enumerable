0239b3641b11b73ec3ea5f7ec4caaa7747e7f2b5
Luckyaremu <aremu.baba70@gmail.com>
2020-03-22 11:23:20 +0100

    emumerables_method complited and errors fixed

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# enumerable
+> Enumerable-Methods
+
+## What does it do
+
+This project contains an implementation of some of the Ruby's enumerable methods
+
+## Built With
+
+- Ruby
+- VSCode
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+Clone the project unto your local machine cd to the folder run "ruby enumerable.rb" on your terminal
+
+### Prerequisites
+
+- Get ruby latest version installed
+
+### Setup
+
+- Clone the repository on your local machine
+- cd into the folder, and open it
+
+### Install
+
+- Install VSCode or any code editor you like
+- Install Node JS and nmp(most recents versions)
+- Run this command on your terminal in order to install stylelint: npm install stylelint stylelint-config-recommended --save-dev
+
+## Author
+
+👤 **Author1**
+Aremu Lucky
+
+- Github: [@githubhandle](https://github.com/Luckyaremu)
+- Twitter: [@twitterhandle](@luckyaremu)
+- Linkedin: [linkedin]()
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page]().
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
---
+++ enumerable.rb
@@ -0,0 +1,132 @@
+# rubocop :disable Style/CaseEquality
+
+# my_each
+module Enumerable
+  def my_each
+    return to_enumerable(:my_each) unless my_each_given?
+
+    each do |x|
+      yield x
+    end
+    my_each
+  end
+  # end of my_each
+
+  # #my_each_with_index
+  def my_each_with_index
+    return to_enumerable(:my_each_with_index) unless my_each_with_index_given?
+
+    my_each_with_index do |x|
+      yield x
+    end
+    my_each_with_index
+  end
+  # end of my_each_with_index
+
+  # my_select
+  def my_select
+    return to_enumerable unless my_select_given?
+
+    array = []
+    my_select do |x|
+      array.push(x) if yield(x) == true
+    end
+    array
+  end
+  # end of select
+
+  # my_all
+  def my_all?(reg = nill)
+    if !reg = nill?
+      my_each { |x| return false unless reg === x }
+    elsif block_given?
+      my_each { |x| return false unless yield x }
+    else
+      my_each { |x| return false unless x }
+    end
+    true
+  end
+  # end of my_all
+
+  # my_any
+  def my_any?(reg = nill)
+    if !reg = nill?
+      my_any { |x| return false unless reg === x }
+    elsif block_given?
+      my_any { |x| return false unless yield x }
+    else
+      my_any { |x| return false unless x }
+    end
+    true
+  end
+
+  # end of my_any
+
+  # my_none
+  def my_none?
+    my_each do |item|
+      if block_given?
+        return false if yield item
+      else
+        return false if item
+      end
+    end
+    true
+  end
+  # end of my_none
+
+  # my_count
+  def my_count(num = nill?)
+    counter = 0
+
+    if number
+      my_each { |x| counter += 1 if num == x }
+      counter
+    elsif block_given?
+      my_each { |x| counter += 1 if yield(x) }
+      counter
+    else
+      num
+    end
+  end
+  # end of counter
+
+  # my_map
+  def my_map(proc = nil)
+    return to_enumerable(:my_map) if !block_given? && proc.nil?
+
+    array = []
+    if !proc.nil?
+      my_each do |x|
+        array.push(proc.call(x))
+      end
+    else
+      my_each do |x|
+        array.push(yield(x))
+      end
+    end
+    my_map
+  end
+  # end of map
+
+  # rubocop:disable Metrics/CyclomaticComplexity:
+  # rubocop:disable Metrics/PerceivedComplexity
+
+  # my_inject
+  def my_inject(num = nill?)
+    my_each do |x|
+      if num == nill
+      else
+        num = yield(x, num)
+      end
+    end
+    num
+  end
+end
+# rubocop:enable Metrics/CyclomaticComplexity:
+# rubocop:enable Metrics/PerceivedComplexity
+# rubocop :enable Style/CaseEquality
+def multiply_els(arr)
+  arr.my_inject { |x, num| x * num }
+end
+# end of my_inject

61f403a45d704a8deead36e661d4351bac7373b1
Luckyaremu <aremu.baba70@gmail.com>
2020-03-23 10:49:05 +0100

    my_each error fixed

--- enumerable.rb
+++ enumerable.rb
@@ -1,31 +1,25 @@
 # rubocop :disable Style/CaseEquality
-
-# my_each
 module Enumerable
   def my_each
-    return to_enumerable(:my_each) unless my_each_given?
+    return to_enumerable(:my_each) unless block_given?
 
     each do |x|
       yield x
     end
-    my_each
+    self
   end
-  # end of my_each
 
-  # #my_each_with_index
   def my_each_with_index
-    return to_enumerable(:my_each_with_index) unless my_each_with_index_given?
+    return to_enumerable(:my_each_with_index) unless block_given?
 
     my_each_with_index do |x|
       yield x
     end
-    my_each_with_index
+    self
   end
-  # end of my_each_with_index
 
-  # my_select
   def my_select
-    return to_enumerable unless my_select_given?
+    return to_enumerable unless block_given?
 
     array = []
     my_select do |x|
@@ -33,9 +27,7 @@ module Enumerable
     end
     array
   end
-  # end of select
 
-  # my_all
   def my_all?(reg = nill)
     if !reg = nill?
       my_each { |x| return false unless reg === x }
@@ -46,9 +38,7 @@ module Enumerable
     end
     true
   end
-  # end of my_all
 
-  # my_any
   def my_any?(reg = nill)
     if !reg = nill?
       my_any { |x| return false unless reg === x }
@@ -60,15 +50,10 @@ module Enumerable
     true
   end
 
-  # end of my_any
-
-  # my_none
   def my_none?(reg = nil, &block)
     !my_any?(reg, &block)
   end
-  # end of my_none
 
-  # my_count
   def my_count(num = nill?)
     counter = 0
 
@@ -82,9 +67,7 @@ module Enumerable
       num
     end
   end
-  # end of counter
 
-  # my_map
   def my_map(proc = nil)
     return to_enumerable(:my_map) if !block_given? && proc.nil?
 
@@ -100,9 +83,7 @@ module Enumerable
     end
     my_map
   end
-  # end of map
 
-  # my_inject
   def my_inject(num = nill?)
     my_each do |x|
       if num == nill
@@ -117,4 +98,3 @@ end
 def multiply_els(arr)
   arr.my_inject { |x, num| x * num }
 end
-# end of my_inject

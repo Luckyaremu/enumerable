5e05b13166a31c1360458241c6ca4ff26192c212
Luckyaremu <aremu.baba70@gmail.com>
2020-04-06 17:20:35 +0100

    my_all - my_none review treated

--- enumerable.rb
+++ enumerable.rb
@@ -144,13 +144,12 @@ module Enumerable
   end
 end
 # rubocop: enable Metrics/ModuleLength
-
-arr = [1, 2, 3, 4, 5, 6]
-t = [nil, true, '']
-p arr.each.class == arr.my_each.class
-p t.my_none? == t.none?
-p t.any? == t.my_any?
-p t.each_with_index.class == t.my_each_with_index.class
-p t.count == t.my_count
-p t.map.class == t.my_map.class
-p arr.inject(:+) == arr.my_inject(:+)
+# arr = [1, 2, 3, 4, 5, 6]
+# t = [nil, true, '']
+# p arr.each.class == arr.my_each.class
+# p t.my_none? == t.none?
+# p t.any? == t.my_any?
+# p t.each_with_index.class == t.my_each_with_index.class
+# p t.count == t.my_count
+# p t.map.class == t.my_map.class
+# p arr.inject(:+) == arr.my_inject(:+)
